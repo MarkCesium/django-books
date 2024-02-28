02ccbd98118db90530595e7173cb44254a21ecf0
MarkCesium <cesiummark01@gmail.com>
2024-02-28 21:08:23 +0300

    Add OAuth GitHub registration

--- books/store/migrations/0002_book_author_name.py
+++ books/store/migrations/0002_book_author_name.py
@@ -4,16 +4,15 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('store', '0001_initial'),
+        ("store", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='book',
-            name='author_name',
-            field=models.CharField(default='Author', max_length=255),
+            model_name="book",
+            name="author_name",
+            field=models.CharField(default="Author", max_length=255),
             preserve_default=False,
         ),
     ]
--- books/store/views.py
+++ books/store/views.py
@@ -1,11 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import render
 from .models import Book
 from .serializers import BooksSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class BookViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]  # Ограничиваем доступ для не авторизованных
+
     queryset = Book.objects.all()  # Запрос в БД, забираем все объекты модели Book
     serializer_class = BooksSerializer  # Указываем класс сериализация для данной модели
 
@@ -16,3 +20,7 @@ class BookViewSet(ModelViewSet):
         "price",
         "author_name",
     ]  # /book/?ordering=-... - сортировка наоборот
+
+
+def auth(request):
+    return render(request, "oauth.html")
